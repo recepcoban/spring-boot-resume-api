33d6a688a400dca6e3f9b1e8359f6b85daecf1a0
recepcoban <recep-coban@yandex.com>
2022-01-22 23:41:12 +0300

    generate project structure

---
+++ src/main/java/com/rcoban/resume/api/SpringBootResumeApiApplication.java
@@ -0,0 +1,13 @@
+package com.rcoban.resume.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResumeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResumeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcoban/resume/api/SpringBootResumeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcoban.resume.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResumeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
