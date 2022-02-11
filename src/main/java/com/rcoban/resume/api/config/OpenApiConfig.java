d58080acdafc26349b44f8f8dffeab742d26a71f
recepcoban <recep-coban@yandex.com>
2022-02-11 21:30:14 +0300

    edit WebMvcConfig

--- src/main/java/com/rcoban/resume/api/config/OpenApiConfig.java
+++ src/main/java/com/rcoban/resume/api/config/OpenApiConfig.java
@@ -15,7 +15,7 @@ public class OpenApiConfig {
         return new OpenAPI().info(new Info()
                 .title("Spring Boot Resume API")
                 .version("0.0.1-SNAPSHOT")
-                .description("Spring Boot Resume API for creating, modifying resumes whatever you need.")
+                .description("Spring Boot Resume API for creating, modifying resume whatever you need.")
                 .license(new License()
                         .name("GNU General Public License v3.0")
                         .url("https://www.gnu.org/licenses/gpl-3.0.html"))
